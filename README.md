079d4ea5f0d0facbde329b23c33ff037f3ffad53
huy923 <huyxam233@gmail.com>
2024-07-12 09:57:14 +0700

    Deploy to Heroku

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # web_node
-# web_node
--- server.js
+++ server.js
@@ -1,27 +1,19 @@
-// server.js
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// Middleware để xử lý các tệp tin tĩnh
 app.use(express.static('public'));
-
-// Route cơ bản
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
-
-// Lắng nghe các yêu cầu
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
-
-app.get('views/dynamic.ejs', (req, res) => {
-    res.send('This is a dynamic route!');
-});
 
 app.set('view engine', 'ejs');
 
-app.get('views/dynamic.ejs', (req, res) => {
+app.get('/dynamic', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/dynamic', (req, res) => {
     res.render('dynamic', { message: 'This is a dynamic page!' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}/dynamic`);
 });
